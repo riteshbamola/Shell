ed3b23ea7bb7584e69fbdd6523b645fe2a084412
Ritesh Bamola <rishubamola01@gmail.com>
2025-05-13 04:53:33 +0530

    Command Parsing Definitions and Prototypes

---
+++ parsing.h
@@ -0,0 +1,11 @@
+// parsing.h - Header for command parsing functions
+// Author: Ritesh Bamola and Team
+
+#ifndef PARSING_H
+#define PARSING_H
+
+// Parsing functions
+void checkInfile(char *tokens);
+void checkOutfile(char *tokens);
+
+#endif
